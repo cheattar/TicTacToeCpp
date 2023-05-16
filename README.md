8f10a034a2639ff6b3a6f1e267ded0194e588adb
Anna Dębowska <cheattar@tuta.io>
2023-05-16 20:08:30 +0200

    mostly working. added win conditons, diagonals slighty broken. tie condition not working. removed warning

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # TicTacToe
+
 This is a very bad Tic-Tac-Toe (or noughts and crosses) type game.
 This is not good code. Pls dont look. Very hacky. Very bad.
 You'll proably cringe at this piece of shite code.
 
 And if for some reason you want to build this code you'll need CMake, and somekind of modern C++ compiler...
-I used Visual Studio 2022 to make this atrocity, but I bet this can run on MSYS or a *NIX system.
+I used Visual Studio 2022 on Windows 10 and Debian 12 with Clang 15 to make this atrocity, but I bet this can run on MSYS or a (non-Linux) *NIX system. (And, yes I've tried GCC it works!)
+
+# TODO:
+
+* Better graphics (VT100, ncurses)
+* AI
+* Clean up code, so it doesn't look *that* bad.
--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -12,14 +12,8 @@ using namespace std;
 */
 
 int playerToInt(bool b) {
-    switch (b) { 
-    case false:
-        return 0;
-    case true:
-        return 1;
-    default:
-        return 3;
-    }    
+    if (b == true) { return 1; }
+    else { return 0; }
 }
 
 int extractInt(string str) {
@@ -47,6 +41,44 @@ void printBoard(vector<vector<int>> board) {
     cout << "+-------+" << endl;
 }
 
+int checkIfWon(vector<vector<int>> board) {
+    // Horizontal
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != 2) {
+            return board[i][0];
+        } 
+    }
+    // Vertical
+    for (int i = 0; i < 3; i++) {
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != 2) {
+            return board[0][i];
+        } 
+    }
+
+    // Diagonals
+	if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 2) {
+			return board[0][0];
+		}
+    if(board[2][0] == board[1][1] && board[1][1] ==  board[0][2] && board[2][0] != 2) {
+			return board[2][0];
+		}
+
+    else {
+        return 2;
+    }
+}
+
+bool checkIfTie(vector<vector<int>> board) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == 2) {
+                    return false;
+            }
+        }
+    }
+    return true;
+}
+
 int main() {
     vector<vector<int>> board{ {2,2,2},
                                {2,2,2},
@@ -55,6 +87,7 @@ int main() {
     bool player = false; // false == naught
                          // true  == cross
     int turns = 0;
+    cout << "Tic Tac Toe!\nInput your your position using A to C for columns, and 1 to 3 for rows.\n";
     while (true) {
         start:
         printBoard(board);
@@ -71,18 +104,40 @@ int main() {
         }
         else {
             switch (input[0]) {
+            case 'a':
             case 'A':
                 if (board[0][extractInt(input)] == 2) { board[0][extractInt(input)] = playerToInt(player); break; }
                 else { cout << "This row is already filled out!\n"; goto start; }
+            case 'b':
             case 'B':
                 if (board[1][extractInt(input)] == 2) { board[1][extractInt(input)] = playerToInt(player); break; }
                 else { cout << "This row is already filled out!\n"; goto start; }
+            case 'c':
             case 'C':
                 if (board[2][extractInt(input)] == 2) { board[2][extractInt(input)] = playerToInt(player); break; }
                 else { cout << "This row is already filled out!\n"; goto start; }
             default:
                 cout << "Please enter a correct row or columb input!\n"; goto start;
             }
+
+            switch (checkIfWon(board)) {
+            case 0:
+                cout << "Naught won the game in " << turns << " turns.\n"; return 0;
+
+            case 1:
+                cout << "Cross won the game in " << turns << " turns.\n"; return 0;
+            case 2:
+                goto addTurn;
+            /*default:
+                continue;*/ 
+            }
+            switch (checkIfTie(board)) {
+            case true:
+                cout << "The game was tied in " << turns << " turns.\n"; return 0;
+            case false:
+                goto addTurn;
+            }
+            addTurn:
             if (turns % 2 == 0) {
                 player = true;
             }
